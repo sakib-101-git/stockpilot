86b8066a05570b414276c393aadb6985f4f5a812
Syed Nazmus Sakib <nazmussak.work@gmail.com>
2026-09-22 23:43:52 +0600

    fix: return the coroutine's result from run_async

--- workers/celery_app.py
+++ workers/celery_app.py
@@ -42,8 +42,7 @@ def run_async(coro):
     try:
         asyncio.get_running_loop()
     except RuntimeError:
-        asyncio.run(coro)
-        return
+        return asyncio.run(coro)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
-        pool.submit(asyncio.run, coro).result()
+        return pool.submit(asyncio.run, coro).result()
